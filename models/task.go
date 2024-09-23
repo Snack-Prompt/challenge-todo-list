42cef4bf7d95f6ea676f181b2442a64b6b948c82
gpessoni <pessonigs@gmail.com>
2024-09-23 13:00:16 -0300

    🗃️ raw: Update the main go from the project for accept connections

--- models/task.go
+++ models/task.go
@@ -5,7 +5,7 @@ import "time"
 type Task struct {
 	ID          string    `gorm:"primaryKey" json:"id"`
 	Title       string    `gorm:"not null" validate:"required,min=3,max=100" json:"title"`
-	Description string    `gorm:"not null" validate:"required,min=10,max=500" json:"description"`
+	Description string    `gorm:"not null" validate:"required,min=3,max=500" json:"description"`
 	Status      string    `gorm:"not null" validate:"required,oneof=pending doing completed" json:"status"`
 	Priority    string    `gorm:"not null;default:'medium'" validate:"required,oneof=low medium high" json:"priority"`
 	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
